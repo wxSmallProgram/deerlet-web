36cc67b56b4e2a69c2d4bb78bddedf921ad7ef93
zhang0135789 <zz634682577@163.com>
2019-06-11 02:25:32 -0700

    init 1

---
+++ script/web.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#git clone https://github.com/wxSmallProgram/deerlet-web.git
+
+cd chain-safe-web
+
+#git checkout -b master-omni origin/master
+
+git pull
+
+cnpm install
+
+npm run build
+
+
